426e823367c5edecf0155bcc02029e45336691d8
agusthryni <11201002@student.itk.ac.id>
2024-05-28 02:31:21 +0800

    updating pool

--- config/db.js
+++ config/db.js
@@ -2,19 +2,24 @@ const dotenv = require("dotenv");
 const mysql = require("mysql2/promise");
 dotenv.config();
 
-const config = {
+const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  connectTimeout: 60000,
-};
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
 
 async function query(sql, params) {
-  const connection = await mysql.createConnection(config);
-  const [results] = await connection.execute(sql, params);
-
-  return results;
+  const connection = await pool.getConnection();
+  try {
+    const [results] = await connection.execute(sql, params);
+    return results;
+  } finally {
+    connection.release();
+  }
 }
 
 module.exports = { query };
