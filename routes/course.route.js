ddb07c0f7484e9ebaf07567ff740bc2b6ff38809
agusthryni <11201002@student.itk.ac.id>
2024-06-02 15:16:20 +0800

    Tambah endpoint course detail

--- routes/course.route.js
+++ routes/course.route.js
@@ -2,6 +2,7 @@ const CourseController = require("../controllers/course.controller");
 const router = require("express").Router();
 
 router.get("/course/:id", CourseController.course);
+router.get("/course/:id/detail", CourseController.courseDetail);
 router.get("/course/test/:id", CourseController.courseTest);
 router.get("/user/course/:id_user", CourseController.userCourse);
 
