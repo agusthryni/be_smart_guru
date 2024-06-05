c350815d982aca40b19e9d8d43603d0927737523
agusthryni <11201002@student.itk.ac.id>
2024-06-05 18:52:20 +0800

    testing submit

--- testing/course.test.js
+++ testing/course.test.js
@@ -1,6 +1,18 @@
 const request = require("supertest");
 const app = "http://besg-test.panti.my.id";
 var courseId;
+var courseQuestions;
+
+function generateRandomAnswers(data) {
+  return data.data.map((question) => {
+    const randomAnswer =
+      question.answers[Math.floor(Math.random() * question.answers.length)];
+    return {
+      id: question.id,
+      answerId: randomAnswer.id,
+    };
+  });
+}
 
 describe("Endpoint Generate Course", () => {
   it(
@@ -45,6 +57,8 @@ describe("Endpoint Course", () => {
     );
     expect(res.body).toHaveProperty("data");
     expect(Array.isArray(res.body.data)).toBe(true);
+
+    courseQuestions = res.body;
   });
 
   it("Gagal mendapatkan data course dengan ID tidak valid [400]", async () => {
@@ -53,6 +67,51 @@ describe("Endpoint Course", () => {
     expect(res.body).toHaveProperty(
       "msg",
       "Course not found or no questions available."
+    );
+  });
+
+  it("Berhasil submit data jawaban dengan ID user, ID course dan jawaban yang valid [200]", async () => {
+    const res = await request(app)
+      .post(`/submit/1`)
+      .send({
+        id_course: courseId,
+        answers: generateRandomAnswers(courseQuestions),
+      });
+    expect(res.statusCode).toEqual(200);
+    expect(res.body).toHaveProperty("msg", "Answers submitted successfully");
+    expect(res.body).toHaveProperty("score");
+  });
+
+  it("Gagal submit data jawaban dengan ID user yang tidak valid [400]", async () => {
+    const res = await request(app)
+      .post(`/submit/999`)
+      .send({
+        id_course: courseId,
+        answers: generateRandomAnswers(courseQuestions),
+      });
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toHaveProperty("msg", "Can't found user with that id");
+  });
+
+  it("Gagal submit data jawaban dengan ID course yang tidak valid [400]", async () => {
+    const res = await request(app)
+      .post(`/submit/1`)
+      .send({
+        id_course: 999999,
+        answers: generateRandomAnswers(courseQuestions),
+      });
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toHaveProperty("msg", "Course not found.");
+  });
+
+  it("Gagal submit data jawaban dengan beberapa argumen kosong [400]", async () => {
+    const res = await request(app).post(`/submit/1`).send({
+      id_course: courseId,
+    });
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toHaveProperty(
+      "msg",
+      "Invalid data format or empty answers array"
     );
   });
 
@@ -70,7 +129,7 @@ describe("Endpoint Course", () => {
   });
 });
 
-describe("Endpoint Course User", () => {
+describe("Course User Endpoint", () => {
   it("Berhasil mendapatkan course - course yang dimiliki oleh user dengan ID yang valid [200]", async () => {
     const res = await request(app).get("/user/course/1");
     expect(res.statusCode).toEqual(200);
@@ -99,7 +158,7 @@ describe("Endpoint Course Stats", () => {
 
   it("Gagal mendapatkan statistik course dengan ID tidak valid [500]", async () => {
     const res = await request(app).get("/course/999/stats");
-    expect(res.statusCode).toEqual(500);
-    expect(res.body).toHaveProperty("msg", "Failed to fetch course statistics");
+    expect(res.statusCode).toEqual(400);
+    expect(res.body).toHaveProperty("msg", "Course not found.");
   });
 });
